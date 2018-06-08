080c1394e52ce2b7bb07304bd0e3dee68de1453c
Jhett Black <jhettblack@hotmail.com>
2018-06-09 01:05:32 +1000

    Copy native lzham dll on build.

---
+++ Forgelight WorldBuilder/Assets/WorldBuilder/Editor/BuildDlls.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_STANDALONE
+namespace WorldBuilder.Editor
+{
+    using UnityEditor;
+    using UnityEditor.Build;
+    using UnityEditor.Build.Reporting;
+
+    public class BuildDlls : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            BuildTarget target = report.summary.platform;
+            string pathToBuiltProject = report.summary.outputPath;
+
+            switch (target)
+            {
+                case BuildTarget.StandaloneWindows64:
+                    FileUtil.ReplaceFile("lzham-forgelightx64.dll", System.IO.Path.GetDirectoryName(pathToBuiltProject) + "/lzham-forgelightx64.dll");
+                    break;
+            }
+        }
+    }
+}
+#endif
